5f049663cbc19db67a37efb5cf4289f3fc5a95c2
Antoine Villecroze <Antoine@pc-82.home>
2019-04-27 12:30:34 +0200

    controller debut

---
+++ LD44/Assets/Scripts/CrowdController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdController : MonoBehaviour
+{
+    public static float moveSpeed = 5f;
+    public static float rotationSpeed = 4f;
+    //public float moveAccelerationSpeed;
+
+    public static float rotationInput;
+    public static float moveInput;
+
+    public static bool firing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Action"))
+        {
+            firing = true;
+        }
+        if (Input.GetButtonUp("Action"))
+        {
+            firing = false;
+        }
+
+        rotationInput = Input.GetAxis("Horizontal");
+        moveInput = Input.GetAxis("Vertical");
+    }
+}
